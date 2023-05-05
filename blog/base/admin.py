ca9c2f2573cc0db576e30c233da95020e4986afe
Prithvij2004 <manikandan2200j@gmail.com>
2023-05-05 13:57:22 +0530

    second push

--- blog/base/admin.py
+++ blog/base/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug', 'date_published', 'upvotes')
+    list_filter = ('date_published',)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slugs': ('title',)}
+
+admin.site.register(Post)
+    
--- blog/base/models.py
+++ blog/base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    content = models.TextField(null=False)
+    author  = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_published = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=200)
+    upvotes = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-date_published']
+    
+    def __str__(self):
+        return self.title
+    
